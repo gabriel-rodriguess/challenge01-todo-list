89a985110d52e3cb468b29c89301beff02888beb
Gabriel Rodrigues dos Santos <rodriguesgg36@gmail.com>
2022-10-29 17:30:20 -0300

    added styled-components and theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const globalTheme = {
+  gray100: '#F2F2F2',
+  gray200: '#D9D9D9',
+  gray300: '#808080',
+  gray400: '#333333',
+  gray500: '#262626',
+  gray600: '#1A1A1A',
+  gray700: '#0D0D0D',
+  purple: '#8284FA',
+  purpleDark: '#5E60CE',
+  blue: '#4EA8DE',
+  blueDark: '#1E6F9F',
+  danger: '#E25858'
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={globalTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
